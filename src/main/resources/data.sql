e3a5ac86d8a28e79e30aac127e37c1333325fa1c
choidongkuen <danaver12@daum.net>
2023-06-24 00:20:08 +0900

    [FEAT] user 관련 data.sql 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into USERS (user_id, created_date, email, major, nickname, role, univ)
+values (1, current_timestamp, 'danaver12@Daum.net', 'major', 'nickname', 1, 'univ'),
+       (2, current_timestamp, 'shiwow12@Naver.com', '컴공', '동자', 0, '한국외국어대학교');
