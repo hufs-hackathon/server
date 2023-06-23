a207f0b7707ea24586ef90a6e3df23ab7f678e4c
choidongkuen <danaver12@daum.net>
2023-06-24 03:56:33 +0900

    [FEAT] 마이페이지 내가 쓴 게시글 조회 로직 구현 및 Repository 메소드 추가

--- src/main/java/com/hachathon/farmmate/api/domain/repository/MenteeBoardRepository.java
+++ src/main/java/com/hachathon/farmmate/api/domain/repository/MenteeBoardRepository.java
@@ -1,6 +1,7 @@
 package com.hachathon.farmmate.api.domain.repository;
 
 import com.hachathon.farmmate.api.domain.entity.MenteeBoard;
+import com.hachathon.farmmate.api.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,6 @@ public interface MenteeBoardRepository extends JpaRepository<MenteeBoard, Long> 
 
     @Query("SELECT m FROM MenteeBoard m INNER JOIN User u ON u.univ = :univ AND m.category = :category ORDER BY m.createdDate DESC")
     List<MenteeBoard> findAllByUnivAndCategoryOrderByCreatedDateDesc(@Param("univ") String univ, @Param("category") String category);
+
+    List<MenteeBoard> findAllByUser(User user);
 }
--- src/main/java/com/hachathon/farmmate/api/domain/repository/MentorBoardRepository.java
+++ src/main/java/com/hachathon/farmmate/api/domain/repository/MentorBoardRepository.java
@@ -1,10 +1,13 @@
 package com.hachathon.farmmate.api.domain.repository;
 
 import com.hachathon.farmmate.api.domain.entity.MentorBoard;
+import com.hachathon.farmmate.api.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MentorBoardRepository extends JpaRepository<MentorBoard, Long> {
     List<MentorBoard> findAllByCategory(String category);
+
+    List<MentorBoard> findAllByUser(User user);
 }
