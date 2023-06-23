c029f1b4679f714d0818eafcc16cb16fe72acd6f
choidongkuen <danaver12@daum.net>
2023-06-24 00:21:13 +0900

    [FEAT] 멘토 게시글 & 멘티 게시글 등록 API 구현

--- src/main/java/com/hachathon/farmmate/api/service/MenteeBoardService.java
+++ src/main/java/com/hachathon/farmmate/api/service/MenteeBoardService.java
@@ -1,9 +1,53 @@
 package com.hachathon.farmmate.api.service;
 
+import com.hachathon.farmmate.api.domain.entity.MenteeBoard;
+import com.hachathon.farmmate.api.domain.entity.MenteeImage;
+import com.hachathon.farmmate.api.domain.entity.User;
+import com.hachathon.farmmate.api.domain.repository.MenteeBoardRepository;
+import com.hachathon.farmmate.api.domain.repository.MenteeImageRepository;
+import com.hachathon.farmmate.api.domain.repository.UserRepository;
+import com.hachathon.farmmate.api.dto.request.RegisterMenteeBoardRequestDto;
+import com.hachathon.farmmate.exception.CustomException;
+import com.hachathon.farmmate.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MenteeBoardService {
+
+    private final MenteeBoardRepository menteeBoardRepository;
+    private final MenteeImageRepository menteeImageRepository;
+    private final UserRepository userRepository;
+    private final S3Service s3Service;
+
+    @Transactional
+    public Long registerMenteeBoard(Long userId, RegisterMenteeBoardRequestDto request, List<MultipartFile> s3Images) {
+
+        User user = this.userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        MenteeBoard menteeBoard = this.menteeBoardRepository.save(
+                MenteeBoard.builder()
+                        .title(request.getTitle())
+                        .content(request.getContent())
+                        .category(request.getCategory())
+                        .user(user)
+                        .build()
+        );
+
+        s3Images.stream()
+                .map(s3Service::uploadImage)
+                .map(imageUrl -> MenteeImage.builder()
+                        .menteeBoard(menteeBoard)
+                        .imageUrl(imageUrl)
+                        .build())
+                .forEach(this.menteeImageRepository::save);
+
+        return menteeBoard.getId();
+    }
 }
